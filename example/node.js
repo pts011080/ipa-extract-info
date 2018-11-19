f05fa52046be2cae3abf82f605c4426589103e0c
Louis Tsang <hmtsang@midland.com.hk>
2018-11-19 11:06:59 +0800

    Commit: get icon 2018-11-19 Louis

--- example/node.js
+++ example/node.js
@@ -4,7 +4,33 @@ var extract = require('..');
 var ipa = process.argv[2];
 var fd = fs.openSync(ipa, 'r');
 
-extract(fd, function(err, info, raw){
-  if (err) throw err;
-  console.log(info);
+// extract(fd, function(err, info, raw){
+//   // if (err) throw err;
+//   // console.log(info);
+// });
+
+var ipa = new extract(fd);
+
+ipa.info(ipa.fd,function(err,info,src){
+	if (err) {
+		console.log('err: ',err);
+	}else{
+		console.log('info: ',info);
+	}
+});
+
+ipa.icon(ipa.fd,function(err,icon){
+	if (err) {
+		console.log('err: ',err);
+	}else{
+		console.log('icon: ',icon);
+		
+		fs.writeFile("./icon.png", icon,  "binary",function(err) {
+	    if(err) {
+	        console.log(err);
+	    } else {
+	        console.log("The file was saved!");
+	    }
+		});
+	}
 });
